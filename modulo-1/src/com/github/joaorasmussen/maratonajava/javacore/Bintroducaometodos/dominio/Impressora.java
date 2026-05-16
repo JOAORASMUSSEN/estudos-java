fc0a7bb76855bd81dfd1de9c8b64ae6ce4d0aa5d
João Rasmussen <joao.rasmussen.rezende@gmail.com>
2026-05-16 15:01:45 -0300

    docs: comentários adicionado na classe impressora

--- modulo-1/src/com/github/joaorasmussen/maratonajava/javacore/Bintroducaometodos/dominio/Impressora.java
+++ modulo-1/src/com/github/joaorasmussen/maratonajava/javacore/Bintroducaometodos/dominio/Impressora.java
@@ -2,6 +2,7 @@ package com.github.joaorasmussen.maratonajava.javacore.Bintroducaometodos.domini
 
 public class Impressora {
     public void imprime(Estudante estudante){
+        //parâmetro passado via referência
         System.out.println(estudante.nome);
         System.out.println(estudante.idade);
         System.out.println(estudante.genero);
