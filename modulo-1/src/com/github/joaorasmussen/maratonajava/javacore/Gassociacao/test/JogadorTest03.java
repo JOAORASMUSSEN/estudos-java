3efbfc1d83942c151f92a4c65a155f33e2abdb20
João Rasmussen <joao.rasmussen.rezende@gmail.com>
2026-07-21 19:24:43 -0300

    feat: implementa associacao bidirecional na classe Time e adiciona JogadorTest03

---
+++ modulo-1/src/com/github/joaorasmussen/maratonajava/javacore/Gassociacao/test/JogadorTest03.java
@@ -0,0 +1,29 @@
+package com.github.joaorasmussen.maratonajava.javacore.Gassociacao.test;
+
+import com.github.joaorasmussen.maratonajava.javacore.Gassociacao.dominio.Jogador;
+import com.github.joaorasmussen.maratonajava.javacore.Gassociacao.dominio.Time;
+
+public class JogadorTest03 {
+    public static void main(String[] args) {
+        Jogador jogador1 = new Jogador("Kaká");
+        Jogador jogador2 = new Jogador("Rogério Ceni");
+        Time time = new Time("SPFC");
+        Jogador[] jogadores = {jogador1, jogador2};
+
+        jogador1.setTime(time);
+        jogador2.setTime(time);
+        time.setJogadores(jogadores);
+
+        System.out.println("---Jogador---");
+
+        jogador1.imprime();
+
+        System.out.println("---Jogador2---");
+        //se não adicionar o time em jogador2 não imprime
+        jogador2.imprime();
+
+        System.out.println("---Time---");
+
+        time.imprime();
+    }
+}
